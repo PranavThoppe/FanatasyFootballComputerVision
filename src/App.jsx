d3ae666a988b6cdb8e7b2a392d11a9f94d793eaa
pthoppe <prv.pats@gmail.com>
2024-11-24 18:00:13 -0600

    Add initial React setup with CheckMap component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CheckMap from './components/CheckMap';
+
+function App() {
+  return (
+    <div>
+      <CheckMap />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CheckMap.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CheckMap = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default CheckMap;
